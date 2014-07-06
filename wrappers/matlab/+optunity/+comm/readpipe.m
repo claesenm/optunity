b88d744eae65a30bf32db34eb908548459abbb13
Marc Claesen <claesenm@gmail.com>
2014-07-06 20:36:19 +0200

    Update readpipe.m

--- wrappers/matlab/+optunity/+comm/readpipe.m
+++ wrappers/matlab/+optunity/+comm/readpipe.m
@@ -1,5 +1,5 @@
 function [ content ] = readpipe( pipe )
-%READPIPE Reads data chunks from SOAP over its stdout pipe.
+%READPIPE Reads data chunks from Optunity over its stdout pipe.
 
 content = char(pipe.readLine());
 if numel(content) == 0
