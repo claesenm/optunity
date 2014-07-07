3049d887dcb0acdbac6163d75a0fd24c4d0d8af3
Marc Claesen <claesenm@gmail.com>
2014-07-07 16:28:26 +0200

    simple example of R wrapper

---
+++ wrappers/R/example.R
@@ -0,0 +1,15 @@
+source('optunity.R')
+
+
+## fold generation
+strata <- list(c(1,2,3), c(6,7,8,9))
+folds <- generate_folds(20, num_folds=10, num_iter=2, strata=strata)
+
+
+## solver manual request
+manual()
+
+## solver demo
+f <- function(x, y) return (-x^2-y^2)
+cfg <- list(x = seq(-5, 5, by=0.5), y = seq(-5, 5, by=0.5))
+result <- solve('grid-search', cfg, f, return_call_log=TRUE)
