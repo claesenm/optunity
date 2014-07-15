9f4f27ffb62a9acce97e1f5d2288bc4c2633e336
Marc Claesen <claesenm@gmail.com>
2014-07-15 21:44:18 +0200

    bugfix in docs

--- docs/conf.py
+++ docs/conf.py
@@ -14,12 +14,12 @@
 
 import sys
 import os
-import optunity
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #sys.path.insert(0, os.path.abspath('.'))
+sys.path.insert(0, os.path.abspath("../optunity/"))
 
 # -- General configuration ------------------------------------------------
 
