8711d412193ef99ae2bd0095f0b0503283039aa0
Jaak Simm <jaak.simm@gmail.com>
2014-08-21 18:37:54 +0200

    cv run

--- wrappers/R/R/crossvalidation.R
+++ wrappers/R/R/crossvalidation.R
@@ -1,17 +1,23 @@
 
-cv.setup <- function(x, y=NULL, num_folds=5, num_iter=1, 
+cv.setup <- function(x, y=NULL, score, num_folds=5, num_iter=1, 
                      strata=NULL, clusters=NULL,
                      seed=NULL) {
   if ( ! is.matrix(x) && ! is.data.frame(x))
     stop("x has to be either a matrix or data.frame")
   
+  if (missing(score))
+    stop("Please provide score function(ytrue, yscore), like score.accuracy for classification or score.neg.mse for regression.")
+  
   setup <- list()
+  setup$x = x
+  setup$y = y
+  setup$score = score
+  setup$num_folds = num_folds
+  setup$num_iter  = num_iter
   setup$supervised = ! is.null(y)
   if (setup$supervised && nrow(x) != length(y))
     stop( sprintf("Number of rows of x is not equal to the length of y.", nrow(x), length(y)) )
-  setup$x = x
-  setup$y = y
-  setup$num_folds = num_folds
+  
   setup$seed = seed
   if ( ! is.null(seed) ) set.seed(seed)
   
@@ -22,24 +28,64 @@ cv.setup <- function(x, y=NULL, num_folds=5, num_iter=1,
 }
 
 
-cv.run <- function(setup, f, ...) {
+cv.run <- function(setup, f, ..., maximize = TRUE) {
   if ( ! inherits(setup, "cv.setup"))
     stop("Input setup has to be of class 'cv.setup'. Use cv.setup() to create it.")
   
-  results = sapply(1:nrows(setup$folds), function(iter) {
+  scores = sapply(1:setup$num_iter, function(iter) {
     sapply(1:setup$num_folds, function(fold) {
-      itrain = setup$folds[setup$folds != fold]
-      itest  = setup$folds[setup$folds == fold]
+      itrain = setup$folds[,iter] != fold
+      itest  = ! itrain
       xtrain = setup$x[ itrain, ]
       xtest  = setup$x[ itest,  ]
       ytrain = setup$y[ itrain ]
       ytest  = setup$y[ itest  ]
-      score <- f(xtrain, ytrain, xtest, ytest, ...)
-      if (length(score) > 1) stop("f returned a vector, but should return 1 numeric value.")
-      if ( ! is.numeric(score)) stop("f returned non-numeric value.")
-      return(score)
+      yhat <- f(xtrain, ytrain, xtest, ...)
+      if (is.null(setup$score)) {
+        s <- yhat
+        if (length(s) > 1) stop("f returned a vector, but should return 1 numeric value (score).")
+        if ( ! is.numeric(s)) stop("f returned non-numeric value.")
+      } else {
+        s <- setup$score(ytest, yhat)
+      }
+      return(s)
     })
   })
-  results = vector("numeric")
-  mean(results)
+  out <- list()
+  out$score.mean = mean(scores)
+  out$score.sd   = sd(scores)
+  out$scores     = scores
+  return( out )
 }
+
+score.neg.mse <- function(ytrue, yhat) {
+  - mean((ytrue-yhat)^2)
+}
+
+score.accuracy <- function(ytrue, yhat) {
+  mean(ytrue==yhat)
+}
+
+score.roc.auc <- function(ytrue, yscore, decreasing=TRUE, top=1.0) {
+  enrichvs::auc(yscore, ytrue, decreasing=decreasing, top=top)
+}
+
+score.pr.auc <- function(ytrue, yscore, decreasing=TRUE) {
+  perf <- ROCR::performance(ROCR::prediction(yscore, ytrue), "prec", "rec")
+  prec <- perf@y.values[[1]]
+  if (is.na(prec[1]))
+    prec[1] <- 1.0
+  rec  <- perf@x.values[[1]]
+  aver <- (prec[-1] + prec[-length(prec)]) / 2
+  return( sum(base::diff(rec) * aver) )
+}
+
+score.rie <- function(ytrue, yscore, decreasing=TRUE, alpha=20.0) {
+  enrichvs::rie(yscore, ytrue, decreasing=decreasing, alpha=alpha)
+}
+
+score.bedroc <- function(ytrue, yscore, decreasing=TRUE, alpha=20.0) {
+  enrichvs::bedroc(yscore, ytrue, decreasing=TRUE, alpha=20.0)
+}
+
+
--- wrappers/R/inst/tests/testthat/test-cv.r
+++ wrappers/R/inst/tests/testthat/test-cv.r
@@ -5,8 +5,18 @@ x <- matrix(runif(50*5), 50, 5)
 y <- x[,1] + 0.5*x[,2] + 0.1*runif(50)
 
 test_that("cv.setup can be created", {
-  cv <- cv.setup(x, y, num_folds = 10, num_iter = 2)
+  cv <- cv.setup(x, y, score=score.neg.mse, num_folds = 10, num_iter = 2)
   expect_equal( cv$supervised, TRUE )
   expect_equal( nrow(cv$folds), 50 )
   expect_equal( ncol(cv$folds), 2 )
+})
+
+test_that("cv.run works with regression", {
+  cv <- cv.setup(x, y, score=score.neg.mse, num_folds = 5, num_iter = 2)
+  ## linear regression (w-o intercept)
+  regr <- function(x, y, xtest) {
+    beta = solve(t(x) %*% x, t(x) %*% y)
+    xtest %*% beta
+  }
+  
 })
