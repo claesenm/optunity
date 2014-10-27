3fecb0164aae39cafbadeaccd6d76c860ac177f0
Marc Claesen <claesenm@gmail.com>
2014-10-27 14:48:05 +0100

    added disclaimer to test

--- bin/test-piped.py
+++ bin/test-piped.py
@@ -1,8 +1,45 @@
+#! /usr/bin/env python
+
+# Author: Marc Claesen
+#
+# Copyright (c) 2014 KU Leuven, ESAT-STADIUS
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+#
+# 1. Redistributions of source code must retain the above copyright
+# notice, this list of conditions and the following disclaimer.
+#
+# 2. Redistributions in binary form must reproduce the above copyright
+# notice, this list of conditions and the following disclaimer in the
+# documentation and/or other materials provided with the distribution.
+#
+# 3. Neither name of copyright holders nor the names of its contributors
+# may be used to endorse or promote products derived from this software
+# without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+# ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+# A PARTICULAR PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR
+# CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+# EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+# PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+# PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+# LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+# NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+# SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+# This executable is meant to debug optunity.piped across platforms.
+# It launches optunity as any alien environment would and communicates
+# over sockets.
+
 from __future__ import print_function
 import subprocess
 import socket
 import sys
-
 
 # prepare server socket
 print('Making server socket.')
@@ -20,7 +57,7 @@ sock = serv_sock.accept()[0]
 __channel_in = sock.makefile('r')
 __channel_out = sock.makefile('w')
 
-# check if we want to run a custom command
+
 if len(sys.argv) > 1:
     msg = sys.argv[1]
 else:
