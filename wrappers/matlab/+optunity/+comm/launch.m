fe43a33486cf39de4316f743b8ca74ce756f0229
Marc Claesen <claesenm@gmail.com>
2014-10-30 09:22:28 +0100

    launcher.m now uses platform-independent symbols, should work on windows

--- wrappers/matlab/+optunity/+comm/launch.m
+++ wrappers/matlab/+optunity/+comm/launch.m
@@ -3,36 +3,38 @@ function [ m2py, py2m, stderr, handle, cleaner ] = launch()
 %   Optunity is launched through a Java Runtime().exec() call.
 %   To enable Optunity to locate installed libraries and necessary
 %   dependencies, we must pass Python's paths explicitly to the subprocess
-%   environment.
+%   optunity_environment.
 %
 %   This function returns stdin, stdout, stderr and a handler of the
 %   Optunity subprocess. 
 %   Destruction of the subprocess must be done manually.
 
 % hack to fix empty paths when spawning Optunity
-% find the current path used by python
-% necessary to pass as an env variable when launching Optunity
-persistent env
-persistent path_to_bin
-if isempty(env)
-    path = mfilename('fullpath');
-    path = path(1:strfind(path, '/wrappers/matlab/+optunity/+comm/launch'));
-    path_to_bin = [path,'/bin/'];
+% find the current optunity_pathused by python
+% necessary to pass as an optunity_env variable when launching Optunity
+persistent optunity_env
+% persistent optunity_path
+if isempty(optunity_env)
+    optunity_path = mfilename('fullpath');
+    f = filesep;
+    hit = [f, 'wrappers', f, 'matlab', f, '+optunity', f, '+comm', f, 'launch'];
+    optunity_path= optunity_path(1:strfind(optunity_path, hit));
+    path_to_bin = [optunity_path, f, 'bin'];
     
-    [~, pathstr] = system([path_to_bin, 'print_system_path.py']);
+    [~, pathstr] = system(['python ', path_to_bin, f, 'print_system_path.py']);
     pathstr = pathstr(pathstr ~= '''');
     pathstr = pathstr(pathstr ~= '[');
     pathstr = pathstr(pathstr ~= ']');
-    pathstr = strrep(pathstr, ', ',':');
-    env = ['PYTHONPATH=', pathstr];
+    pathstr = strrep(pathstr, ', ', pathsep);
+    optunity_env = ['PYTHONPATH=', strtrim(pathstr)];
     
-    % attach optunity's path to env
-    env = [env, ':', path];
+    % attach optunity's path to optunity_env
+    optunity_env = [strtrim(optunity_env), pathsep, strtrim(optunity_path)];
 end
 
 % cmd = [path_to_bin, 'run_optunity_piped.py'];
 cmd = 'python -m optunity.piped';
-[m2py, py2m, handle, socket] = optunity.comm.popen( cmd, env );
+[m2py, py2m, handle, socket] = optunity.comm.popen( cmd, optunity_env );
 
 % provide RAII-style automatic cleanup when cleaner goes out of scope
 % e.g. both upon normal caller exit or an error
