7034ceb814dd6167e686a0fbda60a807a5f42db1
Marc Claesen <claesenm@gmail.com>
2014-11-19 13:34:34 +0100

    documentatoring

--- optunity/solvers/ParticleSwarm.py
+++ optunity/solvers/ParticleSwarm.py
@@ -117,14 +117,7 @@ class ParticleSwarm(Solver):
         >>> solver.num_generations
         5
 
-        .. include:: /global.rst
-
         .. warning:: |warning-unconstrained|
-
-        .. warning:: This solver is not explicitly constrained. The box constraints that are given
-            are used to initialise the swarm, but particles may leave the specified region during iterations.
-            If this is unacceptable, you must manually constrain the domain of the objective function
-            prior to using this solver (cfr. :doc:`/user/constraints`).
 
         """
 
